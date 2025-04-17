d91f06db573fbe91fa04c3f3f95953f0c5b48780
Yeonsu <yeunsu0707@gmail.com>
2025-04-18 01:45:04 +0900

    DB추가

---
+++ backend/musimco.sql
@@ -0,0 +1,66 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: musimco
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `zipcode` varchar(10) NOT NULL,
+  `addr` varchar(200) NOT NULL,
+  `addr_detail` varchar(200) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `height` float DEFAULT NULL,
+  `weight` float DEFAULT NULL,
+  `exercise_goal` varchar(100) DEFAULT NULL,
+  `meal_day` tinyint DEFAULT NULL,
+  `exercise_weak` tinyint DEFAULT NULL,
+  `exercise_day` tinyint DEFAULT NULL,
+  `activity_level` varchar(50) DEFAULT NULL,
+  `usertype` enum('개인회원','강사','사업자') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('hgd123','scrypt:32768:8:1$zPv2h44cf67qUYS4$576338b158b245ef5c52683f44a04b8ba65d59c2ca5d57f45d8cb6369293fcfb65d52b80587a607f834034341d68eb11f38c1f0a38b8b06a8fca21cd768b6bd5','홍길동','12345','서울 강남구 테헤란로 123','101동 203호','02-123-4567','010-1234-5678','gd@example.com',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'개인회원');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-16  2:00:05
