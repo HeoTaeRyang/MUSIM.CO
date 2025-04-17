c72defdbee1c0b7f33cafa587a45b70fbf184c54
HeoTaeRyang <109972461+HeoTaeRyang@users.noreply.github.com>
2025-04-18 05:11:13 +0900

    Update Signup.tsx

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,6 +1,6 @@
 
 
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import "../styles/Signup.css";
 import { useNavigate } from "react-router-dom";
 
