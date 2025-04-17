95c4ec14287ff135be17aa4dfdfb730bfe28cf12
hmunseong00 <hms3519@naver.com>
2025-04-18 04:29:15 +0900

    ㅡ

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -109,277 +109,291 @@ const Signup = () => {
 
   return (
     <div className="signup-frame">
-      <div className="signup-title" style={{padding:"80px"}}>회원가입</div>
+      <div className="signup-title" style={{ padding: "80px" }}>회원가입</div>
+  
       <div className="align-left">회원인증</div>
-      < hr/>
+      <hr />
+  
       <table>
         <tr>
           <td>
-      <div className="label member-type-label" style={{ padding: "10px" }}>
-        <span className="required">*</span>
-        <span>회원구분</span>
-        <label style={{ margin: "50px" }}></label>
-        <label style={{ margin: "50px" }}>
-          <input
-            type="radio"
-            value="personal"
-            checked={memberType === "personal"}
-            onChange={handleMemberTypeChange}
-          />
-          개인 회원
-        </label>
-        <label style={{ margin: "50px" }}>
-          <input
-            type="radio"
-            value="instructor"
-            checked={memberType === "instructor"}
-            onChange={handleMemberTypeChange}
-          />
-          강사
-        </label>
-        <label style={{ margin: "50px" }}>
-          <input
-            type="radio"
-            value="business"
-            checked={memberType === "business"}
-            onChange={handleMemberTypeChange}
-          />
-          사업자
-        </label>
-      </div>
-      </td>
+            <div className="label member-type-label" style={{ padding: "10px" }}>
+              <span className="required">*</span>
+              <span>회원구분</span>
+              <label style={{ margin: "50px" }}></label>
+              <label style={{ margin: "50px" }}>
+                <input
+                  type="radio"
+                  value="personal"
+                  checked={memberType === "personal"}
+                  onChange={handleMemberTypeChange}
+                />
+                개인 회원
+              </label>
+              <label style={{ margin: "50px" }}>
+                <input
+                  type="radio"
+                  value="instructor"
+                  checked={memberType === "instructor"}
+                  onChange={handleMemberTypeChange}
+                />
+                강사
+              </label>
+              <label style={{ margin: "50px" }}>
+                <input
+                  type="radio"
+                  value="business"
+                  checked={memberType === "business"}
+                  onChange={handleMemberTypeChange}
+                />
+                사업자
+              </label>
+            </div>
+          </td>
         </tr>
       </table>
-      <hr/>
-    
-      <div className="align-left" style={{fontSize:"18px"}}>기본정보</div>
+  
+      <hr />
+  
+      <div className="align-left" style={{ fontSize: "18px" }}>기본정보</div>
+  
       <div className="required-info-text-right">
-        </div>
-
-      <div className = "required-info-text-right">
-      <span className="required">*</span>
-        <span className="required-text" style={{fontSize:"18px"}}>필수입력사항</span>
-        </div>
-
-      <hr/>
-      <table>
-      <tr>
-      <td>
-        <div className="label id-label" style={{width: "150px"}}>
         <span className="required">*</span>
-        <span>아이디</span>
-        </div>
-      </td>
-      
-      <td style={{ width: "700px" }}>
-      <div className="input-box id-input">
-        <input type="text" value={id} onChange={handleIdChange} />
+        <span className="required-text" style={{ fontSize: "18px" }}>필수입력사항</span>
       </div>
-      <div className="id-rules">(영문소문자/숫자, 4~16자)</div>
-      </td>
-        </tr>
-      </table>
-      <hr/>
+  
+      <hr />
+  
+      {/* 아이디 */}
       <table>
         <tr>
-         <td><div className="label password-label" style={{width: "150px"}}>
-        <span className="required">*</span>
-        <span>비밀번호</span>
-      </div></td>
-      
-      <td style={{ width: "700px" }}>
-      <div className="input-box password-input" >
-        <input type="password" value={password} onChange={handlePasswordChange} />
-      </div>
-      <div className="password-rules">
-        (영문 대소문자/숫자/특수문자 총 2가지 이상 조합, 10자~16자)
-      </div>
-      </td>
+          <td>
+            <div className="label id-label" style={{ width: "150px" }}>
+              <span className="required">*</span>
+              <span>아이디</span>
+            </div>
+          </td>
+          <td style={{ width: "700px" }}>
+            <div className="input-box id-input">
+              <input type="text" value={id} onChange={handleIdChange} />
+            </div>
+            <div className="id-rules">(영문소문자/숫자, 4~16자)</div>
+          </td>
         </tr>
       </table>
-      <hr/>
+  
+      <hr />
+  
+      {/* 비밀번호 */}
       <table>
         <tr>
-         <td style={{width: "150px"}}>
-         <div className="label confirm-password-label">
-        <span className="required">*</span>
-        <span>비밀번호 확인</span>
-      </div>
-         </td>
-      
-      <td style={{ width: "700px" }}>
-      <div className="input-box confirm-password-input">
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-        />
-      </div>
-      </td>
-      </tr>
+          <td>
+            <div className="label password-label" style={{ width: "150px" }}>
+              <span className="required">*</span>
+              <span>비밀번호</span>
+            </div>
+          </td>
+          <td style={{ width: "700px" }}>
+            <div className="input-box password-input">
+              <input type="password" value={password} onChange={handlePasswordChange} />
+            </div>
+            <div className="password-rules">
+              (영문 대소문자/숫자/특수문자 총 2가지 이상 조합, 10자~16자)
+            </div>
+          </td>
+        </tr>
       </table>
-      <hr/>
-
+  
+      <hr />
+  
+      {/* 비밀번호 확인 */}
       <table>
         <tr>
-         <td>
-         <div className="label name-label" style={{width:"150px"}}>
-        <span className="required">*</span>
-        <span>이름</span>
-        </div>
-         </td>
-      <td style={{ width: "700px" }}>
-      <div className="input-box name-input">
-        <input type="text" value={name} onChange={handleNameChange} />
-      </div>
-      </td>
+          <td style={{ width: "150px" }}>
+            <div className="label confirm-password-label">
+              <span className="required">*</span>
+              <span>비밀번호 확인</span>
+            </div>
+          </td>
+          <td style={{ width: "700px" }}>
+            <div className="input-box confirm-password-input">
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+              />
+            </div>
+          </td>
         </tr>
       </table>
-      <hr/>
-
+  
+      <hr />
+  
+      {/* 이름 */}
       <table>
         <tr>
-         <td><div className="label address-label" style={{width:"150px"}}>
-        <span className="required">*</span>
-        <span>주소</span>
-      </div></td>
-      
-      <td style={{ width: "700px" }}>
-  {/* zipCode input + 주소검색 버튼 묶는 flex 컨테이너 */}
-  <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-    <div className="input-box zipcode-input">
-      <input
-        type="text"
-        value={zipCode}
-        onChange={handleZipCodeChange}
-        readOnly
-      />
-    </div>
-    <div className="address-search-button" onClick={handleSearchAddress}>
-      주소검색
-    </div>
-  </div>
-
-  {/* 주소 입력 1 */}
-  <div className="input-box address1-input">
-    <input
-      type="text"
-      value={address1}
-      onChange={handleAddress1Change}
-      readOnly
-    />
-  </div>
-
-  {/* 주소 입력 2 */}
-  <div className="input-box address2-input">
-    <input
-      type="text"
-      value={address2}
-      onChange={handleAddress2Change}
-    />
-  </div>
-      </td>
+          <td>
+            <div className="label name-label" style={{ width: "150px" }}>
+              <span className="required">*</span>
+              <span>이름</span>
+            </div>
+          </td>
+          <td style={{ width: "700px" }}>
+            <div className="input-box name-input">
+              <input type="text" value={name} onChange={handleNameChange} />
+            </div>
+          </td>
         </tr>
       </table>
-
-    <hr/>
-   
-    <table>
-        <tr>
-         <td>
-         <div className="label phone-label" style={{width:"150px"}}>일반전화</div>
-         </td>
-      <td style={{ width: "150px" }}>
-      <div className="input-box phone1-input">
-        <select value={tel1} onChange={handleTel1Change}>
-          <option value="02">02</option>
-          <option value="031">031</option>
-          {/* ... 기타 지역번호 ... */}
-        </select>
-      </div>
-      </td>
-      -
-      <td style={{ width: "150px" }}>
-      <div className="input-box phone2-input">
-        <input type="number" value={tel2} onChange={handleTel2Change} />
-      </div>
-      </td>
-      -
-      <td style={{ width: "150px" }}>
-      <div className="input-box phone3-input">
-        <input type="number" value={tel3} onChange={handleTel3Change} />
-      </div>
-      </td>
-        </tr>
-      </table>
-      <hr/>
+  
+      <hr />
+  
+      {/* 주소 */}
       <table>
         <tr>
-         <td>
-         <div className="label mobile-label" style={{ width: "150px" }}>
-        <span className="required">*</span>
-        <span>휴대전화</span>
-      </div>
-         </td>
-      <td style={{ width: "150px" }}>
-      <div className="input-box mobile1-input">
-        <select value={mobile1} onChange={handleMobile1Change}>
-          <option value="010">010</option>
-          <option value="011">011</option>
-          {/* ... 기타 이동통신 번호 ... */}
-        </select>
-      </div>
-      </td>
-      -
-      <td style={{ width: "150px" }}>
-      <div className="input-box mobile2-input">
-        <input type="number" value={mobile2} onChange={handleMobile2Change} />
-      </div>
-      </td>
-      -
-      <td style={{ width: "150px" }}>
-      <div className="input-box mobile3-input">
-        <input type="number" value={mobile3} onChange={handleMobile3Change} />
-      </div>
-      </td>
+          <td>
+            <div className="label address-label" style={{ width: "150px" }}>
+              <span className="required">*</span>
+              <span>주소</span>
+            </div>
+          </td>
+          <td style={{ width: "700px" }}>
+            <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+              <div className="input-box zipcode-input">
+                <input
+                  type="text"
+                  value={zipCode}
+                  onChange={handleZipCodeChange}
+                  readOnly
+                />
+              </div>
+              <div className="address-search-button" onClick={handleSearchAddress}>
+                주소검색
+              </div>
+            </div>
+            <div className="input-box address1-input">
+              <input
+                type="text"
+                value={address1}
+                onChange={handleAddress1Change}
+                readOnly
+              />
+            </div>
+            <div className="input-box address2-input">
+              <input
+                type="text"
+                value={address2}
+                onChange={handleAddress2Change}
+              />
+            </div>
+          </td>
         </tr>
       </table>
-
-      <hr/>
-
+  
+      <hr />
+  
+      {/* 일반전화 */}
       <table>
         <tr>
-        <td>
-          <div className="label email-label" style={{width:"150px"}}>
-        <span className="required">*</span>
-        <span>이메일</span>
-      </div>
-         </td>
-      <td style={{ width: "700px" }}>
-      <div className="input-box email-input">
-        <input type="email" value={email} onChange={handleEmailChange} />
-      </div>
-      </td>
+          <td>
+            <div className="label phone-label" style={{ width: "150px" }}>
+              일반전화
+            </div>
+          </td>
+          <td style={{ width: "150px" }}>
+            <div className="input-box phone1-input">
+              <select value={tel1} onChange={handleTel1Change}>
+                <option value="02">02</option>
+                <option value="031">031</option>
+                {/* ... 기타 지역번호 ... */}
+              </select>
+            </div>
+          </td>
+          -
+          <td style={{ width: "150px" }}>
+            <div className="input-box phone2-input">
+              <input type="number" value={tel2} onChange={handleTel2Change} />
+            </div>
+          </td>
+          -
+          <td style={{ width: "150px" }}>
+            <div className="input-box phone3-input">
+              <input type="number" value={tel3} onChange={handleTel3Change} />
+            </div>
+          </td>
         </tr>
       </table>
-    <hr/>
-
-    <table style={{ display: 'block', margin: '0 auto' }}>
-      <tr>
-      <td style={{width:"450px", height:"100px"}}>
-        <div className="cancel-button" onClick={handleCancel}>
-        취소
-        </div>
-        </td>
-        <td style={{width:"450px", height:"100px"}}>
-        <div className="signup-button" onClick={handleSubmit}>
-        가입하기
-      </div>
-        </td>
-      </tr>
-    </table>
+  
+      <hr />
+  
+      {/* 휴대전화 */}
+      <table>
+        <tr>
+          <td>
+            <div className="label mobile-label" style={{ width: "150px" }}>
+              <span className="required">*</span>
+              <span>휴대전화</span>
+            </div>
+          </td>
+          <td style={{ width: "150px" }}>
+            <div className="input-box mobile1-input">
+              <select value={mobile1} onChange={handleMobile1Change}>
+                <option value="010">010</option>
+                <option value="011">011</option>
+                {/* ... 기타 이동통신 번호 ... */}
+              </select>
+            </div>
+          </td>
+          -
+          <td style={{ width: "150px" }}>
+            <div className="input-box mobile2-input">
+              <input type="number" value={mobile2} onChange={handleMobile2Change} />
+            </div>
+          </td>
+          -
+          <td style={{ width: "150px" }}>
+            <div className="input-box mobile3-input">
+              <input type="number" value={mobile3} onChange={handleMobile3Change} />
+            </div>
+          </td>
+        </tr>
+      </table>
+  
+      <hr />
+  
+      {/* 이메일 */}
+      <table>
+        <tr>
+          <td>
+            <div className="label email-label" style={{ width: "150px" }}>
+              <span className="required">*</span>
+              <span>이메일</span>
+            </div>
+          </td>
+          <td style={{ width: "700px" }}>
+            <div className="input-box email-input">
+              <input type="email" value={email} onChange={handleEmailChange} />
+            </div>
+          </td>
+        </tr>
+      </table>
+  
+      <hr />
+  
+      {/* 버튼 */}
+      <table style={{ display: "block", margin: "0 auto" }}>
+        <tr>
+          <td style={{ width: "450px", height: "100px" }}>
+            <div className="cancel-button" onClick={handleCancel}>취소</div>
+          </td>
+          <td style={{ width: "450px", height: "100px" }}>
+            <div className="signup-button" onClick={handleSubmit}>가입하기</div>
+          </td>
+        </tr>
+      </table>
     </div>
   );
+  
 };
 
 export default Signup;
