01706c4ca31fd8533a1b55560b2e275c61e87898
song nanji <164975747+songnanji@users.noreply.github.com>
2025-05-30 00:48:06 +0900

    홈 화면의 포인트 랭킹

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import "../styles/Signup.css";
 import { useNavigate } from "react-router-dom";
 
