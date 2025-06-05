8621ee50b566f991af3baae44a53ee8b4de5940e
HeoTeaRyang <cake0702@naver.com>
2025-06-05 18:55:21 +0900

    테스트 코드 추가

--- backend/ai/test.py
+++ backend/ai/test.py
@@ -15,7 +15,7 @@ def read_video_to_frames(path):
     cap.release()
     return frames
 
-def save_frames_to_video(frames, output_path, fps=15):
+def save_frames_to_video(frames, output_path, fps=1):
     if not frames:
         print("저장할 프레임이 없습니다.")
         return
@@ -36,18 +36,20 @@ video_frames = read_video_to_frames("test.mp4")
 # 자세 평가 운동마다 num_conditions 수정 필요
 result = evaluate(
     frames=video_frames,
-    num_conditions=5,
-    model_path="backend/ai/1/1.pth"
+    num_conditions=3,
+    model_path="29/29.pth"
 )
 
-save_frames_to_video(result["annotated_frames"], "test_output.mp4", fps=15)
+save_frames_to_video(result["annotated_frames"], "test_output.mp4", fps=30)
+
+print(result["results"])
 
 
-# #데일리 미션 크런치
-# frame = cv2.imread("test.jpg")
-# angle = get_crunch_angle_from_frame(frame)
+#데일리 미션 크런치
+frame = cv2.imread("test.jpg")
+angle = get_crunch_angle_from_frame(frame)
 
-# if angle is not None:
-#     print(f"각도: {angle:.2f}°")
-# else:
-#     print("관절을 찾을 수 없습니다.")
+if angle is not None:
+    print(f"각도: {angle:.2f}°")
+else:
+    print("관절을 찾을 수 없습니다.")
