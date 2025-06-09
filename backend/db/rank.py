5eca80c637441d7225b0ace54b3129dd7aa2dadc
yoonwootak <ywt0100@naver.com>
2025-06-10 00:57:23 +0900

    마이페이지, 랭킹 백 코드 추가

--- backend/db/rank.py
+++ backend/db/rank.py
@@ -9,3 +9,8 @@ def get_rank_top5():
     with get_connection().cursor() as cursor:
         cursor.execute("SELECT * FROM (SELECT id, username, point, RANK() OVER (ORDER BY point DESC) AS `rank` FROM User) ranked WHERE `rank` <=5;")
         return cursor.fetchall()
+    
+def get_rank_all():
+    with get_connection().cursor() as cursor:
+        cursor.execute("SELECT id, username, point, RANK() OVER (ORDER BY point DESC) AS `rank` FROM User;")
+        return cursor.fetchall()
